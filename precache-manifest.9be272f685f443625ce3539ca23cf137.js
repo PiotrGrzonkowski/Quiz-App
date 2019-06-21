9c5ba6b2ea687dbca12fc19168f949ecb82f3744
Piotr <piotr.grzonkowski88@gmail.com>
2019-06-21 15:39:58 +0200

    Updates

---
+++ precache-manifest.9be272f685f443625ce3539ca23cf137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5074af092be355efd2bddee65bd0e8",
+    "url": "/Quiz-App/index.html"
+  },
+  {
+    "revision": "b00b7905afdeeaea468b",
+    "url": "/Quiz-App/static/css/main.49d4b763.chunk.css"
+  },
+  {
+    "revision": "d27dcead68faee6d2b37",
+    "url": "/Quiz-App/static/js/2.44949046.chunk.js"
+  },
+  {
+    "revision": "b00b7905afdeeaea468b",
+    "url": "/Quiz-App/static/js/main.dff142b5.chunk.js"
+  },
+  {
+    "revision": "c30db239273d780c0bee",
+    "url": "/Quiz-App/static/js/runtime~main.9f56f2ee.js"
+  },
+  {
+    "revision": "2c9ab5cc7ccf2599d6b08f25e446d874",
+    "url": "/Quiz-App/static/media/everest.2c9ab5cc.jpg"
+  }
+]);
